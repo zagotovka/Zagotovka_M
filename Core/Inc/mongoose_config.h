104265814459b92e322972317dd7ccf1a1c1b341
zerg <stm32zagotovka@gmail.com>
2026-04-18 16:07:00 +0300

    Memory leaks fixed - 3!

--- Core/Inc/mongoose_config.h
+++ Core/Inc/mongoose_config.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define MG_TLS MG_TLS_BUILTIN // HTTPS
-//#define TLS_TWOWAY // Двусторонняя аутентификация
+// #define TLS_TWOWAY // Двусторонняя аутентификация
 
 #define MG_ENABLE_CUSTOM_RANDOM 1
 #define MG_ARCH MG_ARCH_NEWLIB
@@ -18,11 +18,11 @@
 #define MG_STMPACK_ARCH MG_ARCH_NEWLIB
 #endif
 
-#define MG_ENABLE_PACKED_FS  1
-#define MG_ENABLE_MBEDTLS  0
+#define MG_ENABLE_PACKED_FS 1
+#define MG_ENABLE_MBEDTLS 0
 #define MG_ARCH MG_ARCH_CMSIS_RTOS2
 #define MG_STMPACK_NET 0
-#define MG_ENABLE_CUSTOM_RANDOM  1
+#define MG_ENABLE_CUSTOM_RANDOM 1
 
 // Translate to Mongoose macros
 #if MG_STMPACK_NET == 0
--- Core/Src/syscalls.c
+++ Core/Src/syscalls.c
@@ -21,25 +21,25 @@
  */
 
 /* Includes */
-#include <sys/stat.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <stdio.h>
 #include <signal.h>
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/times.h>
+#include <time.h>
 
-#include <stdint.h>
 #include "FreeRTOS.h"
+#include "main.h"
 #include "task.h"
+#include <stdint.h>
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
 extern int __io_getchar(void) __attribute__((weak));
 
-
-char *__env[1] = { 0 };
+char *__env[1] = {0};
 char **environ = __env;
 
 /*
@@ -47,167 +47,141 @@ char **environ = __env;
  * It allocates from the linker-defined heap area instead of FreeRTOS heap,
  * because newlib's sbrk strictly requires physically contiguous memory!
  */
-void *_sbrk(ptrdiff_t incr)
-{
-    extern uint8_t _end; /* Symbol defined in the linker script */
-    extern uint8_t _estack; /* Symbol defined in the linker script */
-    extern uint32_t _Min_Stack_Size; /* Symbol defined in the linker script */
-    const uint32_t stack_limit = (uint32_t)&_estack - (uint32_t)&_Min_Stack_Size;
-    const uint8_t *max_heap = (uint8_t *)stack_limit;
-    static uint8_t *__sbrk_heap_end = NULL;
-    uint8_t *prev_heap_end;
+void *_sbrk(ptrdiff_t incr) {
+  extern uint8_t _end;
+  extern uint8_t _estack;
+  extern uint32_t _Min_Stack_Size;
 
-    taskENTER_CRITICAL(); /* Thread safety */
+  const uint32_t stack_limit = (uint32_t)&_estack - (uint32_t)&_Min_Stack_Size;
+  const uint8_t *max_heap = (uint8_t *)stack_limit;
+  static uint8_t *__sbrk_heap_end = NULL;
+  uint8_t *prev_heap_end;
 
-    /* Initialize heap end at first call */
-    if (NULL == __sbrk_heap_end)
-    {
-        __sbrk_heap_end = &_end;
-    }
+  /* PRIMASK — корректен и из задач FreeRTOS, и из ISR любого уровня */
+  uint32_t primask = __get_PRIMASK();
+  __disable_irq();
 
-    /* Protect heap from growing into the reserved MSP stack */
-    if (__sbrk_heap_end + incr > max_heap)
-    {
-        taskEXIT_CRITICAL();
-        errno = ENOMEM;
-        return (void *)-1;
-    }
+  if (NULL == __sbrk_heap_end)
+    __sbrk_heap_end = &_end;
 
-    prev_heap_end = __sbrk_heap_end;
-    __sbrk_heap_end += incr;
+  if (__sbrk_heap_end + incr > max_heap) {
+    __set_PRIMASK(primask);
+    errno = ENOMEM;
+    return (void *)-1;
+  }
 
-    taskEXIT_CRITICAL();
-    return (void *)prev_heap_end;
+  prev_heap_end = __sbrk_heap_end;
+  __sbrk_heap_end += incr;
+
+  __set_PRIMASK(primask);
+  return (void *)prev_heap_end;
 }
 
 /* Functions */
-void initialise_monitor_handles()
-{
-}
+void initialise_monitor_handles() {}
 
-int _getpid(void)
-{
-  return 1;
-}
+int _getpid(void) { return 1; }
 
-int _kill(int pid, int sig)
-{
+int _kill(int pid, int sig) {
   (void)pid;
   (void)sig;
   errno = EINVAL;
   return -1;
 }
 
-void _exit (int status)
-{
+void _exit(int status) {
   _kill(status, -1);
-  while (1) {}    /* Make sure we hang here */
+  while (1) {
+  } /* Make sure we hang here */
 }
 
-__attribute__((weak)) int _read(int file, char *ptr, int len)
-{
+__attribute__((weak)) int _read(int file, char *ptr, int len) {
   (void)file;
   int DataIdx;
 
-  for (DataIdx = 0; DataIdx < len; DataIdx++)
-  {
+  for (DataIdx = 0; DataIdx < len; DataIdx++) {
     *ptr++ = __io_getchar();
   }
 
   return len;
 }
 
-__attribute__((weak)) int _write(int file, char *ptr, int len)
-{
+__attribute__((weak)) int _write(int file, char *ptr, int len) {
   (void)file;
   int DataIdx;
 
-  for (DataIdx = 0; DataIdx < len; DataIdx++)
-  {
+  for (DataIdx = 0; DataIdx < len; DataIdx++) {
     __io_putchar(*ptr++);
   }
   return len;
 }
 
-int _close(int file)
-{
+int _close(int file) {
   (void)file;
   return -1;
 }
 
-
-int _fstat(int file, struct stat *st)
-{
+int _fstat(int file, struct stat *st) {
   (void)file;
   st->st_mode = S_IFCHR;
   return 0;
 }
 
-int _isatty(int file)
-{
+int _isatty(int file) {
   (void)file;
   return 1;
 }
 
-int _lseek(int file, int ptr, int dir)
-{
+int _lseek(int file, int ptr, int dir) {
   (void)file;
   (void)ptr;
   (void)dir;
   return 0;
 }
 
-int _open(char *path, int flags, ...)
-{
+int _open(char *path, int flags, ...) {
   (void)path;
   (void)flags;
   /* Pretend like we always fail */
   return -1;
 }
 
-int _wait(int *status)
-{
+int _wait(int *status) {
   (void)status;
   errno = ECHILD;
   return -1;
 }
 
-int _unlink(char *name)
-{
+int _unlink(char *name) {
   (void)name;
   errno = ENOENT;
   return -1;
 }
 
-int _times(struct tms *buf)
-{
+int _times(struct tms *buf) {
   (void)buf;
   return -1;
 }
 
-int _stat(char *file, struct stat *st)
-{
+int _stat(char *file, struct stat *st) {
   (void)file;
   st->st_mode = S_IFCHR;
   return 0;
 }
 
-int _link(char *old, char *new)
-{
+int _link(char *old, char *new) {
   (void)old;
   (void)new;
   errno = EMLINK;
   return -1;
 }
 
-int _fork(void)
-{
+int _fork(void) {
   errno = EAGAIN;
   return -1;
 }
 
-int _execve(char *name, char **argv, char **env)
-{
+int _execve(char *name, char **argv, char **env) {
   (void)name;
   (void)argv;
   (void)env;
